38f122a8edbc229f8c78220e2799f3433b971ece
NICOLAS MALULEKE <nicolusmaluleke@gmail.com>
2020-11-19 23:57:53 +0200

    Framewrk updated to dotnet 5.0

---
+++ tests/Application.UnitTests/Common/CommandTestBase.cs
@@ -0,0 +1,22 @@
+﻿using CodeClinic.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeClinic.Application.UnitTests.Common
+{
+    class CommandTestBase : IDisposable
+    {
+        
+
+        public CommandTestBase()
+        {
+            //_context = ContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            //ContextFactory.Destroy(_context);
+        }
+    }
+}
---
+++ tests/Application.UnitTests/IssueTickets/Commands/CreateIssueTicketTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeClinic.Application.UnitTests.IssueTickets.Commands
+{
+    public class CreateIssueTicketTests
+    {
+
+    }
+}
